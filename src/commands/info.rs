d2d4736eaae48c8f8cd467663b0e792081593759
worthant <b_dvorkin@niuitmo.ru>
2023-11-05 05:01:37 +0300

    :diamond_shape_with_a_dot_inside: Implement close, help, info, oper, rand commands

---
+++ src/commands/info.rs
@@ -0,0 +1,16 @@
+use super::Command;
+
+pub struct InfoCommand;
+
+impl Command for InfoCommand {
+    fn execute(&self) {
+        println!("Project Name: HandyDB");
+        println!("Author: Dvorkin Boris a.k.a worthant");
+        println!("Date of Initialization: <date>");
+        println!("Size of Memory: <size>");
+    }
+
+    fn help(&self) -> String {
+        "The 'info' command displays detailed information about the project, such as its name, author, initialization date, memory usage, and more.".to_string()
+    }
+}
