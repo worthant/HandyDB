dca665f020428e63109e7d63b8e4500158f7ac5a
worthant <b_dvorkin@niuitmo.ru>
2024-01-03 20:59:00 +0300

    :rocket: Holy shit concurrency is so much faster!

--- src/db/kv_store.rs
+++ src/db/kv_store.rs
@@ -1,59 +1,127 @@
-
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use crate::models::KvPair;
+use dashmap::DashMap;
 
 pub struct KvStore {
-    store: Arc<Mutex<HashMap<String, KvPair>>>,
+    store: Arc<DashMap<String, KvPair>>,
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
         KvStore {
-            store: Arc::new(Mutex::new(HashMap::new())),
+            store: Arc::new(DashMap::new()),
         }
     }
 
     pub fn set(&self, key: String, value: String) {
-        let mut store = self.store.lock().unwrap();
-        store.insert(key.clone(), KvPair::new(key, value));
+        self.store
+            .insert(key.clone(), KvPair::new(key.clone(), value));
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        let store = self.store.lock().unwrap();
-        store.get(&key).map(|kv_pair| kv_pair.value.clone())
+    pub fn get(&self, key: &str) -> Option<String> {
+        self.store.get(key).map(|kv_pair| kv_pair.value.clone())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rayon::prelude::*;
+    use threadpool::ThreadPool;
+
+    #[test]
+    fn test_concurrent_threadpool_kv_store_extreme_ram_usage() {
+        let kv_store = Arc::new(KvStore::new());
+        let num_keys = 1_000_000;
+        let num_threads = 4; // Вы можете настроить количество потоков
+        let pool = ThreadPool::new(num_threads);
+
+        for i in 1..=num_keys {
+            let kv_store = kv_store.clone();
+            pool.execute(move || {
+                kv_store.set(i.to_string(), (i + 1).to_string());
+            });
+        }
+
+        pool.join();
+
+        assert_eq!(kv_store.get("100000"), Some("100001".to_string()));
+        assert_eq!(kv_store.get("500000"), Some("500001".to_string()));
+        assert_eq!(kv_store.get("1000000"), Some("1000001".to_string()));
+    }
+
+    #[test]
+    fn test_concurrent_rayon_kv_store_extreme_ram_usage() {
+        let kv_store = Arc::new(KvStore::new());
+        let num_keys = 1_000_000;
+
+        (1..=num_keys).into_par_iter().for_each(|i| {
+            kv_store.set(i.to_string(), (i + 1).to_string());
+        });
+
+        assert_eq!(kv_store.get("100000"), Some("100001".to_string()));
+        assert_eq!(kv_store.get("500000"), Some("500001".to_string()));
+        assert_eq!(kv_store.get("1000000"), Some("1000001".to_string()));
+    }
+
+    #[test]
+    fn test_kv_store_extreme_ram_usage() {
+        let kv_store = Arc::new(KvStore::new());
+        let num_keys = 1_000_000;
+
+        // this loop takes up approximately 1.6 GB
+        for i in 1..=num_keys {
+            kv_store.set(i.to_string(), (i + 1).to_string());
+        }
+
+        assert_eq!(kv_store.get("100000"), Some("100001".to_string()));
+        assert_eq!(kv_store.get("500000"), Some("500001".to_string()));
+        assert_eq!(kv_store.get("1000000"), Some("1000001".to_string()));
+    }
+
+    #[test]
+    fn test_concurrent_kv_store_get_performance() {
+        let kv_store = Arc::new(KvStore::new());
+        let num_keys = 1_000_000;
+
+        // Заполнение KvStore ключами
+        (1..=num_keys).into_par_iter().for_each(|i| {
+            kv_store.set(i.to_string(), (i + 1).to_string());
+        });
+
+        let start_time = std::time::Instant::now();
+        (1..=num_keys).into_par_iter().for_each(|i| {
+            assert_eq!(kv_store.get(&i.to_string()), Some((i + 1).to_string()));
+        });
+        let duration = start_time.elapsed();
+        println!("Concurrent get time: {:?}", duration);
+    }
+
+    #[test]
+    fn test_kv_store_get_performance() {
+        let kv_store = Arc::new(KvStore::new());
+        let num_keys = 1_000_000;
+
+        // Заполнение KvStore ключами
+        (1..=num_keys).into_par_iter().for_each(|i| {
+            kv_store.set(i.to_string(), (i + 1).to_string());
+        });
+
+        let start_time = std::time::Instant::now();
+        for i in 1..=num_keys {
+            assert_eq!(kv_store.get(&i.to_string()), Some((i + 1).to_string()));
+        }
+        let duration = start_time.elapsed();
+        println!("Concurrent get time: {:?}", duration);
+    }
 
     #[test]
     fn test_kv_store_basic_insertion() {
         let kv_store = KvStore::new();
         kv_store.set("key1".to_string(), "value1".to_string());
-    
-        assert_eq!(kv_store.get("key1".to_string()), Some("value1".to_string()));
-        assert_eq!(kv_store.get("key2".to_string()), None);
-    }
 
-    #[test]
-    fn test_kv_store_extreme_ram_usage() {
-        let kv_store = KvStore::new();
-        let num_keys = 10_000_000;
-
-        // this loop takes up approximately 1.6 GB 
-        for i in 1..=num_keys {
-            kv_store.set(i.to_string(), (i+1).to_string());
-            if i % 100_000 == 0 {
-                println!("Inserted {} keys", i);
-            }
-        }       
-
-        assert_eq!(kv_store.get("100000".to_string()), Some("100001".to_string()));
-        assert_eq!(kv_store.get("500000".to_string()), Some("500001".to_string()));
-        assert_eq!(kv_store.get("1000000".to_string()), Some("1000001".to_string()));
+        assert_eq!(kv_store.get("key1"), Some("value1".to_string()));
+        assert_eq!(kv_store.get("key2"), None);
     }
 }
