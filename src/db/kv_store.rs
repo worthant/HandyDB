c47d1170e9ef4f7aa4281fcd4b19c9f0fad9baa7
worthant <b_dvorkin@niuitmo.ru>
2024-01-03 18:41:48 +0300

    :test_tube: Unit tests for kv_store

--- src/db/kv_store.rs
+++ src/db/kv_store.rs
@@ -29,11 +29,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_kv_store() {
+    fn test_kv_store_basic_insertion() {
         let kv_store = KvStore::new();
         kv_store.set("key1".to_string(), "value1".to_string());
-
+    
         assert_eq!(kv_store.get("key1".to_string()), Some("value1".to_string()));
         assert_eq!(kv_store.get("key2".to_string()), None);
+    }
+
+    #[test]
+    fn test_kv_store_extreme_ram_usage() {
+        let kv_store = KvStore::new();
+        let num_keys = 10_000_000;
+
+        // this loop takes up approximately 1.6 GB 
+        for i in 1..=num_keys {
+            kv_store.set(i.to_string(), (i+1).to_string());
+            if i % 100_000 == 0 {
+                println!("Inserted {} keys", i);
+            }
+        }       
+
+        assert_eq!(kv_store.get("100000".to_string()), Some("100001".to_string()));
+        assert_eq!(kv_store.get("500000".to_string()), Some("500001".to_string()));
+        assert_eq!(kv_store.get("1000000".to_string()), Some("1000001".to_string()));
     }
 }
