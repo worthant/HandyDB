64591550be025c42ba1d1b5cb03afa1f152b7cc8
worthant <b_dvorkin@niuitmo.ru>
2024-01-06 04:35:57 +0300

    :rocket: Multithread concurrent CLI & HttpServer

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,47 @@
-use std::sync::{Arc, Mutex};
+use std::{sync::{Arc, Mutex}, thread};
+use actix_web::{HttpServer, App, web};
 use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
 use cli::CommandManager;
 use db::KvStore;
+use crate::network::configure_services;
 
 mod db;
 mod cli;
 mod models;
+mod tests;
+mod network;
 
 fn create_shared_kv_store() -> Arc<Mutex<KvStore>> {
     Arc::new(Mutex::new(KvStore::new()))
 }
 
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     println!("Hello from {}! This HandyDB release was compiled for {}.", CURRENT_PLATFORM, COMPILED_ON);
+
+    // Enable logging
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
 
     // Init our first and last HandyDB instance of this application
     let kv_store = create_shared_kv_store();
 
-    // Init CLI
-    let mut manager = CommandManager::new();
-    manager.setup_commands(kv_store);
-    manager.run();
+    // CLone KvStore for CLI 
+    let cli_kv_store = kv_store.clone();
+    
+    // Run CLI in a separate thread
+    thread::spawn(move || {
+        let mut manager = CommandManager::new();
+        manager.setup_commands(cli_kv_store);
+        manager.run();
+    });
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(web::Data::new(kv_store.clone()))
+        .configure(configure_services)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
